e79ae170dff995ed6eaaa43cadc22e1c775f404a
Stephen Allen <founder@discoversoftwaresolution.com>
2025-06-05 02:14:47 -0400

    Create webpack.config.js

---
+++ src/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development", 
+  entry: "./src/index.js", 
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true, 
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 3000,
+    setupMiddlewares: (middlewares, devServer) => {
+      
+      return middlewares;
+    },
+    hot: true, 
+    historyApiFallback: true, 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", 
+    }),
+  ],
+};
