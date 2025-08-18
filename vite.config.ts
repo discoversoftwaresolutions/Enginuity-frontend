7559a8a4f531c7ddf356120d34093e04f4bc812c
Stephen Allen <founder@discoversoftwaresolution.com>
2025-08-18 10:12:25 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { screenGraphPlugin } from "@animaapp/vite-plugin-screen-graph";
 import react from "@vitejs/plugin-react";
-import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "development" && screenGraphPlugin()],
+  base: "/",
   publicDir: "./static",
-  base: "./",
-  css: {
-    postcss: {
-      plugins: [tailwind()],
-    },
-  },
+  plugins: [
+    react(),
+    ...(mode === "development" ? [screenGraphPlugin()] : [])
+  ],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  }
 }));
