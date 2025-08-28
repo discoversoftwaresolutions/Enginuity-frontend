cc8c9a3340a95a875edb17d95761490877f9ef06
Stephen Allen <founder@discoversoftwaresolution.com>
2025-08-28 14:55:43 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
+// src/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const root = document.getElementById("root");
-if (!root) throw new Error("Root element #root not found");
-
-createRoot(root).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
