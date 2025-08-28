c5ad20f5ca1ed696d403f18dbb4329515fcfc5be
Stephen Allen <founder@discoversoftwaresolution.com>
2025-08-28 14:47:26 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css"; // contains @tailwind base; @tailwind components; @tailwind utilities;
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root");
+if (!root) throw new Error("Root element #root not found");
+
+createRoot(root).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
