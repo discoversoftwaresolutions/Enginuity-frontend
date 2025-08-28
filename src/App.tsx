de6f299e8af44e0aff33b500235da2a32fc02f13
Stephen Allen <founder@discoversoftwaresolution.com>
2025-08-28 15:28:38 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 const Home       = lazy(() => import("./modules/Home"));
 const SimulAI    = lazy(() => import("./modules/SimulAI"));
 const VisuAI     = lazy(() => import("./modules/VisuAI"));
-const ProtoPrint = lazy(() => import("./modules/ProtoPrint"));
+const ProntoPrint = lazy(() => import("./modules/ProtoPrint"));
 const CircuitIQ  = lazy(() => import("./modules/CircuitIQ"));
 const CodeMotion = lazy(() => import("./modules/CodeMotion"));
 const FlowCore   = lazy(() => import("./modules/FlowCore"));
@@ -18,7 +18,7 @@ const App: React.FC = () => (
       <Route path="/" element={<Home />} />
       <Route path="/simulai" element={<SimulAI />} />
       <Route path="/visuai" element={<VisuAI />} />
-      <Route path="/protoprint" element={<ProtoPrint />} />
+      <Route path="/prontoprint" element={<ProtoPrint />} />
       <Route path="/circuitiq" element={<CircuitIQ />} />
       <Route path="/codemotion" element={<CodeMotion />} />
       <Route path="/flowcore" element={<FlowCore />} />
