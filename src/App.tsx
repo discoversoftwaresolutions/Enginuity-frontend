fcf31410df99e0f3463198c2ac4c669988a9380e
Stephen Allen <founder@discoversoftwaresolution.com>
2025-08-28 19:34:46 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
 // src/App.tsx
 import React, { Suspense, lazy } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
 
-const Home       = lazy(() => import("./modules/Home"));
-const SimulAI    = lazy(() => import("./modules/SimulAI"));
-const VisuAI     = lazy(() => import("./modules/VisuAI"));
-const ProtoPrint = lazy(() => import("./modules/ProtoPrint"));
-const CircuitIQ  = lazy(() => import("./modules/CircuitIQ"));
-const CodeMotion = lazy(() => import("./modules/CodeMotion"));
-const FlowCore   = lazy(() => import("./modules/FlowCore"));
-const AeroIQ     = lazy(() => import("./modules/AeroIQ"));
-const FusionX    = lazy(() => import("./modules/FusionX"));
+const Home = lazy(() => import("./modules/Home"));
 
 const App: React.FC = () => (
   <Suspense fallback={<div>Loading…</div>}>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/simulai" element={<SimulAI />} />
-      <Route path="/visuai" element={<VisuAI />} />
-      <Route path="/protoprint" element={<ProtoPrint />} />
-      <Route path="/circuitiq" element={<CircuitIQ />} />
-      <Route path="/codemotion" element={<CodeMotion />} />
-      <Route path="/flowcore" element={<FlowCore />} />
-      <Route path="/aeroiq" element={<AeroIQ />} />
-      <Route path="/fusionx" element={<FusionX />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <Home />
   </Suspense>
 );
 
