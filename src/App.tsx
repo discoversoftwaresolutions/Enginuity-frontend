afcf1959cd26cddf0ab7e9a3e50648c5d3a64036
Stephen Allen <founder@discoversoftwaresolution.com>
2025-08-29 07:23:54 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ModuleLoader from "./ModuleLoader";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold mb-4">Enginuity Modules</h1>
+      {/* Replace "Simulai - Simulation AI" with any valid module name from moduleMap */}
+      <ModuleLoader moduleName="Simulai - Simulation AI" />
+    </div>
+  );
+}
+
+export default App;
