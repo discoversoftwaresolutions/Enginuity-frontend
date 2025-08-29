075bd40e908e01d4c519df11c0088d7fb2a05de0
Stephen Allen <founder@discoversoftwaresolution.com>
2025-08-29 07:35:40 -0400

    Update ModuleLoader.tsx

--- src/modules/ModuleLoader.tsx
+++ src/modules/ModuleLoader.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function ModuleLoader() {
+type Props = {
+  moduleName: string;
+};
+
+export default function ModuleLoader({ moduleName }: Props) {
   return <div />;
 }
