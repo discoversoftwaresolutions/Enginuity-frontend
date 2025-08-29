00cdbb1cd9bae522939dbbada59cc382d400239f
Stephen Allen <founder@discoversoftwaresolution.com>
2025-08-28 20:11:50 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,15 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          850: "#141a22"
+        }
+      }
+    }
+  },
+  plugins: []
 } satisfies Config;
